07feb79894ed946227499714cdd09d21eb39767d
MukkuMayc <st063775@student.spbu.ru>
2019-09-02 22:10:27 +0300

    Update script to build and run js-script on python.server

--- build_emscripten.sh
+++ build_emscripten.sh
@@ -1,4 +1,18 @@
-mkdir _build
+if [ ! -d '_build' ]; then
+    mkdir _build
+fi
 cd _build
+
 emcmake cmake ..
 emmake make -j2
+python3 ../put_arguments.py
+
+cd tinyram/stark-tinyram
+
+PAGE=index.html
+if test -f "$PAGE"; then
+    rm "$PAGE"
+fi
+
+echo -n "<script src='stark-tinyram.js'></script>" >> "$PAGE"
+python3 -m http.server
