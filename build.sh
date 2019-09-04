efa93541a79bfcab346cc31cb340de0aefcaafef
ddsvetlov <dankasvetlov@gmail.com>
2019-09-04 13:16:42 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,22 @@
-mkdir _build
+#create build dir
+if [ ! -d "_build" ]; then
+    mkdir _build
+fi
 cd _build
+
+#run cmake
 cmake ..
-make -j2
+
+#set threads number while compilation
+input=../threads_num.txt
+if [ -f "$input" ]; then
+  while IFS= read -r line || [ -n "$line" ]; do
+    echo "LINE: $line"
+    threads_num="$line"
+  done < "$input"
+else
+  threads_num=2
+fi
+
+#compile
+make -j"$threads_num"
