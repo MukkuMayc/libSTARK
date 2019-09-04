aed77a347385cbe22590a33cabd0c26ddc3f4ca4
MukkuMayc <st063775@student.spbu.ru>
2019-09-04 23:25:20 +0300

    Remove temporary files

--- dd_script/test_string_work.py
+++ dd_script/test_string_work.py
@@ -1,7 +1,7 @@
 def binary_string_to_int(string1):
 	# print(string1)
 	string2 = string1[1:len(string1)-1].split(' ')
-	valueInMemory=0;
+	valueInMemory=0
 	if string2!=['']:
 		for i in range(len(string2)):
 			valueInMemory+=int(string2[i])*pow(2,i)
