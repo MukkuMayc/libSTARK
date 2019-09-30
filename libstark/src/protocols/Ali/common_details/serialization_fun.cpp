1a8fd161a081ee6bceb3fc0f4ede4d5ce1e93518
ddsvetlov <dankasvetlov@gmail.com>
2019-09-30 15:18:46 +0300

    add function for serialize prover'

---
+++ libstark/src/protocols/Ali/common_details/serialization_fun.cpp
@@ -0,0 +1,5 @@
+//
+// Created by daniil on 30.09.2019.
+//
+
+#include "serialization_fun.h"
---
+++ libstark/src/protocols/Ali/common_details/serialization_fun.h
@@ -0,0 +1,14 @@
+//
+// Created by daniil on 30.09.2019.
+//
+
+#ifndef LIBSTARK_SERIALIZATION_FUN_H
+#define LIBSTARK_SERIALIZATION_FUN_H
+
+
+class serialization_fun {
+
+};
+
+
+#endif //LIBSTARK_SERIALIZATION_FUN_H
